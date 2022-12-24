515e6119466ababb46b566da56da64d2ad0bba69
AbuduwufuerAbulimiti <abdughopur02@gmail.com>
2022-12-24 18:41:46 -0500

    i  added new line

--- src/part1/MyFirstClass.java
+++ src/part1/MyFirstClass.java
@@ -3,5 +3,6 @@ package part1;
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("New Story ");
     }
 }
