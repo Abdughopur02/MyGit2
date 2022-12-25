675a5aa1fafd3d93cd5b53d1a10a0661c0c878dc
Abdughopur02 <114306644+Abdughopur02@users.noreply.github.com>
2022-12-24 19:03:52 -0500

    Update SecondClass.java
    additional line is added

--- src/part1/SecondClass.java
+++ src/part1/SecondClass.java
@@ -4,5 +4,7 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println(" we have to push");
+        System.out.println("I will be the best");
+      
     }
 }
