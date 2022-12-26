c1cb8fdcaa5bb4265ceed7566ec436bd3c1ebfde
AbuduwufuerAbulimiti <abdughopur02@gmail.com>
2022-12-26 09:50:30 -0500

    First Attempt file is created

---
+++ src/firstAttempt/First_Attempt.java
@@ -0,0 +1,7 @@
+package firstAttempt;
+
+public class First_Attempt {
+    public static void main(String[] args) {
+        System.out.println("I know I can do it");
+    }
+}
--- src/part1/MyFirstClass.java
+++
@@ -1,8 +0,0 @@
-package part1;
-
-public class MyFirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("New Story ");
-    }
-}
