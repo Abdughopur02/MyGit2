5ecd21bbf8fdc44261f7731515001854598f1985
AbuduwufuerAbulimiti <abdughopur02@gmail.com>
2022-12-26 10:21:42 -0500

    Test2 updated

--- src/part2/Test2.java
+++ src/part2/Test2.java
@@ -3,6 +3,6 @@ package part2;
 public class Test2 {
     public static void main(String[] args) {
 
-        System.out.println("How are you guys ?");
+        System.out.println("How are you Busra ?");
     }
 }
